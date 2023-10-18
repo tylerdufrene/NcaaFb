0d2376a1a60806f5aebf3f1215261050b92a5161
tylerdufrene <tylerdufrene94@gmail.com>
2023-10-18 17:39:36 -0400

    Removing db files

--- sports_data/ncaafb/utilities/config.py
+++
@@ -1,4 +0,0 @@
-constants = {'API_KEY':'Bearer GJsZsc/cfnD0HazvPGY3FnBCuqKWTwQ15lIwBxGx7cRrogAAkWrH+eW4qBuUPCim'}
-
-def get_constants(key):
-    return constants[key]
--- sports_data/ncaafb/utilities/constants.py
+++
@@ -1,2 +0,0 @@
-base_url = 'https://www.sports-reference.com/cfb/'
-teams_url = base_url + 'schools'
